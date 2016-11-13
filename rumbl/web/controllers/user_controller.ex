63b7c929b993ec3e92b1ea89f6cb7a00b023e5ec
Riccardo <riccardo.marotti@gmail.com>
2016-11-13 18:30:01 +0100

    Add hashed passwords

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -18,9 +18,9 @@ defmodule Rumbl.UserController do
   end
   
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
-      {:ok,user} ->
+      {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
